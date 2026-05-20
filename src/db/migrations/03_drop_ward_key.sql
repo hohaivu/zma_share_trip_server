6b260927e614a3c0d2e5f2fad16fe63a02b3af4a
Vu Ho <hohaivu@gmail.com>
2026-05-20 11:26:20 +0700

    Make Ward Key Drop Migration Idempotent

--- src/db/migrations/03_drop_ward_key.sql
+++ src/db/migrations/03_drop_ward_key.sql
@@ -1,11 +1,7 @@
-BEGIN;
-
 ALTER TABLE routes
-  DROP COLUMN origin_ward_key,
-  DROP COLUMN destination_ward_key;
+  DROP COLUMN IF EXISTS origin_ward_key,
+  DROP COLUMN IF EXISTS destination_ward_key;
 
 ALTER TABLE plans
-  DROP COLUMN origin_ward_key,
-  DROP COLUMN destination_ward_key;
-
-COMMIT;
+  DROP COLUMN IF EXISTS origin_ward_key,
+  DROP COLUMN IF EXISTS destination_ward_key;
